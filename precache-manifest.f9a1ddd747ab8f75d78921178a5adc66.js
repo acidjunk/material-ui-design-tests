f00584abcfc5c19ec0de7f97901e1655c7098d93
Rene Dohmen (imac) <acidjunk@gmail.com>
2020-02-19 17:13:19 +0100

    Updates

---
+++ precache-manifest.f9a1ddd747ab8f75d78921178a5adc66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ab3cbcd5438e8a1ae8e5491c40aa0b",
+    "url": "/material-ui-design-tests/index.html"
+  },
+  {
+    "revision": "75dd53294f24af598138",
+    "url": "/material-ui-design-tests/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7f649a8e144dc2fc6327",
+    "url": "/material-ui-design-tests/static/js/2.f1ecfb74.chunk.js"
+  },
+  {
+    "revision": "c50c25a3e91b902a9d4e9aafe6d9baf9",
+    "url": "/material-ui-design-tests/static/js/2.f1ecfb74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75dd53294f24af598138",
+    "url": "/material-ui-design-tests/static/js/main.4fa26cc0.chunk.js"
+  },
+  {
+    "revision": "657f68209248b6bb0857",
+    "url": "/material-ui-design-tests/static/js/runtime-main.c5d17622.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/material-ui-design-tests/static/media/logo.5d5d9eef.svg"
+  }
+]);
